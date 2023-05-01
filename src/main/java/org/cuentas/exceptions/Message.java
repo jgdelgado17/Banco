ac85cb177d91917492a89acf87d041bcef7e4775
jgdelgado17 <jegadele@hotmail.com>
2023-05-01 16:55:33 -0500

    Agregando fecha del error al manejo de excepciones

--- src/main/java/org/cuentas/exceptions/Message.java
+++ src/main/java/org/cuentas/exceptions/Message.java
@@ -1,8 +1,19 @@
 package org.cuentas.exceptions;
 
+import java.time.LocalDateTime;
+
 public class Message {
     private String error;
     private String detail;
+    private LocalDateTime timestamp;
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public String getError() {
         return error;
@@ -24,5 +35,7 @@ public class Message {
         String[] items = message.split("Detail: ");
         setError(items[0].split("ERROR: ")[1]);
         setDetail(items[1]);
+        LocalDateTime now = LocalDateTime.now();
+        setTimestamp(now);
     }
 }
